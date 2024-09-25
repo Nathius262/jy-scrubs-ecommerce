7630c64197ca709a130c8e398f0d7592aba6a553
Nathius262 <egbodonathaniel@yahoo.com>
2024-09-25 09:20:06 +0100

    renamed models extention to cjs

--- models/index.js
+++ models/index.js
@@ -1,43 +1,51 @@
-'use strict';
+import Sequelize from 'sequelize';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import configFile from '../config/config.cjs'; // Adjust based on your config
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = configFile[env];
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// Initialize Sequelize
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs
-  .readdirSync(__dirname)
+// Read and load models
+const modelFiles = fs.readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   });
 
+  const models = await Promise.all(modelFiles.map(async (file) => {
+    try {
+      const modelPath = new URL(file, import.meta.url).href; // Use URL constructor
+      const model = await import(modelPath);
+      return model.default(sequelize, Sequelize.DataTypes);
+    } catch (error) {
+      console.error(`Error loading model ${file}:`, error);
+      throw error; // Re-throw the error for further handling if needed
+    }
+  }));
+  
+
+models.forEach(model => {
+  db[model.name] = model; // Store the model in the db object
+});
+
+// Setup associations
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+// Add sequelize and Sequelize to the db object
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
