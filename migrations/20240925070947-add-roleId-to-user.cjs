f7a5e3ea5afdcdba60e226743132c26bc6ae9d07
Nathius262 <egbodonathaniel@yahoo.com>
2024-09-25 10:06:23 +0100

    added role column  to user table

---
+++ migrations/20240925070947-add-roleId-to-user.cjs
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'roleId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Roles',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'roleId');
+  }
+};
