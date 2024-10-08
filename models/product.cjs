a9f928dc65313a3468bc861d6f2d233466828661
Nathius262 <egbodonathaniel@yahoo.com>
2024-10-08 15:07:00 +0100

    updated product file

--- models/product.cjs
+++ models/product.cjs
@@ -10,11 +10,45 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Many-to-many with Category
       Product.belongsToMany(models.Category, {
-        through: 'ProductCategory',  // Junction table
+        through: 'ProductCategory',
         foreignKey: 'productId',
-        as: 'categories'             // Alias for related categories
+        as: 'categories'
+      });
+
+      // Many-to-many with Size
+      Product.belongsToMany(models.Size, {
+        through: 'ProductSize',
+        foreignKey: 'productId',
+        as: 'sizes'
+      });
+
+      // Many-to-many with Color
+      Product.belongsToMany(models.Color, {
+        through: 'ProductColor',
+        foreignKey: 'productId',
+        as: 'colors'
+      });
+
+      // Many-to-many with Collection
+      Product.belongsToMany(models.Collection, {
+        through: 'ProductCollection',
+        foreignKey: 'productId',
+        as: 'collections'
+      });
+
+      // Many-to-many with Scrub
+      Product.belongsToMany(models.Scrub, {
+        through: 'ProductScrub',
+        foreignKey: 'productId',
+        as: 'scrubs'
+      });
+
+      // One-to-many with Image
+      Product.hasMany(models.Image, {
+        foreignKey: 'productId',
+        as: 'images'
       });
     }
   }
