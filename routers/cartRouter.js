86f5b0f37fe3b7dc3d5ab1e1b4edcf91ff1e5519
Nathaniel Odo <egbodonathaniel@yahoo.com>
2024-12-20 01:23:47 +0100

    updated cart router

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
-import {checkout, exchangeRate, verifyPaystackTransaction} from '../controllers/cartController.js';
+import {checkout, exchangeRate, verifyPaystackTransaction, trackOrder, trackOrderPage} from '../controllers/cartController.js';
 
 
 const router = Router();
 
 router.get('/api/convert-currency', exchangeRate);
 router.get('/api/verify-payment', verifyPaystackTransaction);
+router.get('/api/track-order', trackOrder);
+router.get('/track-order', trackOrderPage);
 
 router.get('/', checkout);
 
